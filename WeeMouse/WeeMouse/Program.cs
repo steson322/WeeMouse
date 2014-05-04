0a59ef5a4864948377706f490afba2ee2532238d
steson322 <stevensong7re@gmail.com>
2014-05-03 17:13:47 -0700

    Initial commit

---
+++ WeeMouse/WeeMouse/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeMouse
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            Service1 service = new Service1();
+            service.StartDebug(null);
+
+            Console.WriteLine("Press Enter to Stop");
+            Console.ReadLine();
+
+            service.StopDebug();
+
+#else
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new Service1() 
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+}
---
+++ WeeMouse/WeeMouse/Service1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeMouse
+{
+    public partial class Service1 : ServiceBase
+    {
+        WeeForm Form;
+
+        public Service1()
+        {
+            InitializeComponent();
+            Form = new WeeForm();
+        }
+
+        public void StartDebug(string[] args)
+        {
+            OnStart(args);
+        }
+
+        public void StopDebug()
+        {
+            OnStop();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Form.Start();
+        }
+
+        protected override void OnStop()
+        {
+            Form.Stop();
+        }
+    }
+}
